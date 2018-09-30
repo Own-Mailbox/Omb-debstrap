81be2f1ad452449b5a5cf0a347305a78523e07b7
Pierre Parent <pierre.parent@pparent.fr>
2018-09-30 21:47:47 +0200

    Add a command to disable systemd-resolved.

--- setup-scripts/required-packages.sh
+++ setup-scripts/required-packages.sh
@@ -21,6 +21,8 @@ apt-get update
 
 apt-get install -y apt-utils apt-transport-https
 apt-get remove -y resolvconf openresolv network-manager
+systemctl disable systemd-resolved.service
+service systemd-resolved stop
 DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 build-essential curl dnsutils git gnupg iptables redsocks iptables-persistent libcurl4-openssl-dev libjpeg-dev libxml2-dev libxslt1-dev mysql-server ntpdate openssh-server openssl postfix postfix-mysql postfix-pcre procmail python-dev python-jinja2 python-lxml python-pgpdump python-pip python-virtualenv rsyslog spambayes sudo tor torsocks wget zlib1g-dev qrencode
 
 apt-get install -y certbot -t jessie-backports
